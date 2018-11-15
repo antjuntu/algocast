d3ab0997651e478ef10f257ac85e4b85f753b8b1
antjuntu <antjuntu@gmail.com>
2018-11-15 13:02:47 +0200

    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,64 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  const chunked = []
+  let index = 0
 
-module.exports = chunk;
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size))
+    index += size
+  }
+  return chunked
+}
+
+// const result =  chunk([1, 2, 3, 4, 5], 10)
+// console.log(result)
+
+module.exports = chunk
+
+// function chunk(array, size) {
+//   const resultArr = []
+//   let i = 0
+//   let subArr = []
+//   while (i < array.length) {
+//     if (subArr.length < size) {
+//       subArr.push(array[i])
+//     } else {
+//       resultArr.push(subArr)
+//       subArr = []
+//       subArr.push(array[i])
+//     }
+//     i++
+//   }
+//   resultArr.push(subArr)
+//   return resultArr
+// }
+
+// function chunk(array, size) {
+//   const chunked = []
+//   array.forEach(element => {
+//     let lastElementChunked = chunked[chunked.length - 1]
+//     if (!lastElementChunked || lastElementChunked.length === size) {
+//       let newChunk = []
+//       newChunk.push(element)
+//       chunked.push(newChunk)
+//     } else {
+//       lastElementChunked.push(element)
+//     }
+//   })
+//   return chunked
+// }
+
+// function chunk(array, size) {
+//   const chunked = []
+//   for (let element of array) {
+//     const last = chunked[chunked.length - 1]
+//     if (!last || last.length === size) {
+//       chunked.push([element])
+//     } else {
+//       last.push(element)
+//     }
+//   }
+//   return chunked
+// }
