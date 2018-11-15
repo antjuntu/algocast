78ffb7f0216626249cd0a5fa27fe313a5dcedd75
antjuntu <antjuntu@gmail.com>
2018-11-15 14:05:24 +0200

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,77 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const aCharMap = buildCharMap(stringA)
+  const bCharMap = buildCharMap(stringB)
 
-module.exports = anagrams;
+  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false
+  }
+
+  for (let char in aCharMap) {
+    if (!bCharMap[char] || bCharMap[char] !== aCharMap[char]) {
+      return false
+    }
+  }
+
+  return true
+}
+
+function buildCharMap(str) {
+  const charMap = {}
+  
+  for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1
+  }
+
+  return charMap
+}
+
+
+const res = anagrams('h..... ello', 'ehllO!!!!! ')
+console.log(res)
+
+module.exports = anagrams
+
+// function anagrams(stringA, stringB) {
+//   const charactersA = {}
+//   for (const char of stringA) {
+//     if (charactersA[char]) {
+//       charactersA[char]++
+//     } else {
+//       charactersA[char] = 1
+//     }
+//   }
+  
+//   let counter = 0
+//   for (const char of stringB) {
+//     if (!charactersA[char] || charactersA[char] < 1) {
+//       return false
+//     }
+//     else {
+//       charactersA[char]--
+//       counter++
+//     }
+//   }
+//   return counter === stringB.length
+//  }
+
+// function anagrams(stringA, stringB) {
+//   if (stringA.length !== stringB.length) {
+//     return false
+//   }
+//   const sortedA = stringA.split('').sort().join('')
+//   //console.log(sortedA)
+//   const sortedB = stringB.split('').sort().join('')
+//   //console.log(sortedB)
+  
+//   let index = 0
+//   while (index < sortedA.length) {
+//     if (sortedA[index] !== sortedB[index]) {
+//       return false
+//     }
+//     index++
+//   }
+//   return true
+//  }
