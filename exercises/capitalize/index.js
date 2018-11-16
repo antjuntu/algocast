d3cae63b07d0f28dbc01b872aaf05c6196d7d868
antjuntu <antjuntu@gmail.com>
2018-11-16 18:22:32 +0200

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,51 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  let result = str[0].toUpperCase()
 
-module.exports = capitalize;
+  for (let i = 1; i < str.length; i++) {
+    if (str[i - 1] === ' ') {
+      result += str[i].toUpperCase()
+    } else {
+      result += str[i]
+    }
+  }
+
+  return result
+}
+
+const result = capitalize('look,  it is working!')
+console.log(result)
+
+module.exports = capitalize
+
+// function capitalize(str) {
+//   const words = []
+  
+//   for (let word of str.split(' ')) {
+//     words.push(word[0].toUpperCase() + word.slice(1))
+//   }
+
+//   return words.join(' ')
+// }
+
+// function capitalize(str) {
+//   let index = 0
+//   const result = []
+//   let previusIsWhiteSpace = true
+
+//   while (index < str.length) {
+//     let char = str[index]
+
+//     if (/\s/.test(char)) {
+//       previusIsWhiteSpace = true
+//     } else if (/[A-Za-z]/.test(char) && previusIsWhiteSpace) {
+//       char = char.toUpperCase()
+//       previusIsWhiteSpace = false
+//     }
+//     result.push(char)
+//     index++
+//   }
+//   return result.join('')
+// }
