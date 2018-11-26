6a8fbdd64afed29bc0d39af56c0fea467f5c4ee2
antjuntu <antjuntu@gmail.com>
2018-11-26 18:04:39 +0200

    vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,48 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+  const matches = str.match(/[aeiou]/gi)
+  return matches ? matches.length : 0
+}
 
-module.exports = vowels;
+vowels('antti')
+
+module.exports = vowels
+
+// function vowels(str) {
+//   let count = 0
+//   const letters = ['a', 'e', 'i', 'o', 'u']
+//   for (let char of str.toLowerCase()) {
+//     if (letters.includes(char)) {
+//       count++
+//     }
+//   }
+
+//   return count
+// }
+
+// function vowels(str) {
+//   let counter = 0
+//   let vowels = { a: 1, e: 1, i: 1, o: 1, u: 1 }
+//   for (let char of str) {
+//     if (char.toLowerCase() in vowels) {
+//       counter++
+//     }
+//   }
+//   return counter
+// }
+
+// function vowels(str) {
+//   let counter = 0
+//   let vovels = ['a', 'e', 'i', 'o', 'u']
+//   for (let char of str) {
+//     for (v of vovels) {
+//       if (v === char.toLowerCase()) {
+//         counter++
+//         break
+//       }
+//     }
+//   }
+//   return counter
+// }
